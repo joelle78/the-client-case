0ebadd90217ab3ca27bde6c4daccbc17613c107c
joelle78 <96306220+joelle78@users.noreply.github.com>
2022-10-10 23:46:54 +0200

    Reserverings form toegevoegd

---
+++ styles/script.js
@@ -0,0 +1,9 @@
+// Pop up form
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
